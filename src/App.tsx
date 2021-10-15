334d05a9b69e60f5e0392c494ac0f380ef47c674
Pablo Pardo <pablopardoalcocer@gmail.com>
2021-10-15 18:48:49 -0400

    Add announcement form page

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,21 @@ import { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import './index.css';
+import "./index.css";
 
 import AnnouncementsList from "./components/AnnouncementsList";
+import AnnouncementsForm from "./components/AnnouncementsForm";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <Switch>
-          <Route path="/" component={AnnouncementsList}>
-
+          <Route exact path="/">
+            <AnnouncementsList />
+          </Route>
+          <Route exact path="/announcement-form">
+            <AnnouncementsForm />
           </Route>
         </Switch>
       </div>
